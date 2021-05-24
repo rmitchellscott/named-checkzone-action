6264ff1c048d99c53a1957b5541002f7f34214ea
Mitchell Scott <10804314+rmitchellscott@users.noreply.github.com>
2021-05-24 11:57:16 -0500

    ci tshooting

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,8 +2,10 @@
 failure=0
 for file in zones/*.zone ; do
     zoneName=`ls $file | sed 's/\.[^.]*$//' | awk -F'/' '{print $2}'`
-    #oneName=`ls $file | sed 's/\.[^.]*$//'` 
-    echo "named-checkzone $file $zoneName"
+    if [ $? -ne 1 ]; then
+      failure=0
+    fi
+    named-checkzone $file $zoneName
     if [ $? -ne 1 ]; then
       failure=0
     fi
